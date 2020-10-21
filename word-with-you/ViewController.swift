691144b4bc88ced8e8cbd8f0ad4e13855955ae87
sethpoly <emailspolyniak@gmail.com>
2020-10-21 09:21:28 -0400

    First commit

---
+++ word-with-you/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  word-with-you
+//
+//  Created by Seth Polyniak on 10/21/20.
+//  Copyright © 2020 Seth Polyniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
