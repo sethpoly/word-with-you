f07cd0a8c6748c3e2aa2b4b18083023887f3761b
sethpoly <emailspolyniak@gmail.com>
2020-10-25 08:54:37 -0400

    Organized folder structure, started devotionView

---
+++ word-with-you/Views/DevotionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DevotionViewController.swift
+//  word-with-you
+//
+//  Created by Seth Polyniak on 10/25/20.
+//  Copyright © 2020 Seth Polyniak. All rights reserved.
+
+//  Controller for the dynamic devotion screen of the app. This view consists of an {image} that covers the entire
+//  background, an {audio file} that is controlled by various playback buttons, a {transcript} that is the audio
+//  text, a {like button} that adds this devotion to the user's liked list
+
+import UIKit
+
+class DevotionViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        
+    }
+
+    
+    
+    
+}
